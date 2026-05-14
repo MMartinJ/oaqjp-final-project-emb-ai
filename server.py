478381fcc36ca2762714d2602fe332db75b05e69
MMartinJ <marina.m.jurado86@gmail.com>
2026-05-14 19:45:17 -0400

    task 8 completed

--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+"""
+Executing this function initiates the application for emotion
+detection to be executed over the Flask channel and deployed on
+localhost:5000
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,19 +11,27 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """
+    This code receives the text from the HTML interface and
+    runs emotion detection over it using emotion_detector()
+    function.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
-
     response = emotion_detector(text_to_analyze)
-
     dominant_emotion = response['dominant_emotion']
-
     if dominant_emotion is None:
         return "Invalid text! Please try again."
-        
-    return f"For the given statement, the system response is {response}. The dominant emotion is {dominant_emotion}."
+    return (
+        f"For the given statement, the system response is {response}."
+        f"The dominant emotion is {dominant_emotion}."
+    )
 
 @app.route("/")
 def render_index_page():
+    """
+    This function initiates the rendering of the main application
+    page over the Flask channel
+    """
     return render_template('index.html')
 
 
